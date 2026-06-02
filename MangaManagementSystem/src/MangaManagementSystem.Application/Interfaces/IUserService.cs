5de5dbe564cbe4d187452caba77895d7a862a4e4
Hoang Phan <congli2301@gmail.com>
2026-06-02 13:33:23 +0700

    Add ActivateUser and DisableUser methods to IUserService

--- MangaManagementSystem/src/MangaManagementSystem.Application/Interfaces/IUserService.cs
+++ MangaManagementSystem/src/MangaManagementSystem.Application/Interfaces/IUserService.cs
@@ -5,12 +5,16 @@ using System.Threading.Tasks;
 namespace MangaManagementSystem.Application.Interfaces
 {
     public interface IUserService
-    {
-        Task<UserDto> CreateUserAsync(CreateUserDto dto);
-        Task<UserDto?> GetUserByIdAsync(int id);
-        Task<UserDto?> GetUserByEmailAsync(string email);
-        Task<IEnumerable<UserDto>> GetUsersByStatusAsync(string status);
-        Task<UserDto> ApproveUserAsync(int userId);
-        Task RejectUserAsync(int userId);
-    }
+{
+    Task<UserDto> CreateUserAsync(CreateUserDto dto);
+    Task<UserDto?> GetUserByIdAsync(int id);
+    Task<UserDto?> GetUserByEmailAsync(string email);
+    Task<IEnumerable<UserDto>> GetUsersByStatusAsync(string status);
+
+    Task<UserDto> ApproveUserAsync(int userId);
+    Task RejectUserAsync(int userId);
+
+    Task<UserDto> ActivateUserAsync(int userId);
+    Task<UserDto> DisableUserAsync(int userId);
+}
 }
