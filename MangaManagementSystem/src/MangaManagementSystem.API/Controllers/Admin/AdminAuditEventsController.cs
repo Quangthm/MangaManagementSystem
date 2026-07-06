95bd51d7d9e242ab2fe537f8ecff429e8368955c
Quangthm <qtang889706@gmail.com>
2026-07-06 13:23:09 +0700

    Move auth and admin docs to the appropriate place, delete old legacy code

--- MangaManagementSystem/src/MangaManagementSystem.API/Controllers/Admin/AdminAuditEventsController.cs
+++ MangaManagementSystem/src/MangaManagementSystem.API/Controllers/Admin/AdminAuditEventsController.cs
@@ -7,7 +7,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MangaManagementSystem.API.Controllers
+namespace MangaManagementSystem.API.Controllers.Admin
 {
     [ApiController]
     [Authorize(Roles = "Admin")]
